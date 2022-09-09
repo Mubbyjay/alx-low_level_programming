5055ad056c1ae76eedae690231167432ad5f96f3
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-09 15:03:23 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z';)
+	{
+		if (alpha == 'q')
+		{
+			alpha++;
+		}
+		else if (alpha == 'a')
+		{
+			alpha++;
+		}
+		else
+		{
+			putchar(alpha);
+			alpha++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
