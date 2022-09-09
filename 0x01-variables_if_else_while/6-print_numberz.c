b729710b00a21060894e567e705ba66bee095e3b
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-09 15:24:59 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int mub = '0';
+
+	for (num = '0'; num <= '9'; mum++)
+		putchar(num);
+
+	putchar('\n');
+	return (0);
+}
