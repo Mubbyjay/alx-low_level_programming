bfce7a6bc9131df33b3ce4a6cdd8245dd299f60f
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-09 15:44:30 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mub = 'z';
+
+	for (mub = 'z'; mub >= 'a'; mub--)
+		putchar(mub);
+
+	putchar('\n')
+
+	return (0);
+}
