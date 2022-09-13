983ae1b0f14a48c6a0719b8bfee7ff8389895d20
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-13 09:08:28 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+_putchar('\n');
+}
