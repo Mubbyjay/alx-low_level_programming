7653b77ae1fc5d827efd1d7f671fb57c813ab042
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-28 13:18:21 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (0);
+
+	i++;
+	i += _strlen_recursion(s + 1);
+	return (i);
+}
