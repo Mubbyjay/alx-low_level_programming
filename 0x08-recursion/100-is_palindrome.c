e04e374f2dd6ce856e0190d4c257d560ac437f07
Mubbyjay <mubaraqyusuff@gmail.com>
2022-09-28 13:50:32 -0700

    Task 8 update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,3 +17,36 @@ int _strlen_recursion(char *s)
 	}
 	return (sum);
 }
+
+/**
+ * check - checks for palindrome
+ * @s: string to be checked
+ * @start: start index
+ * @end: end index
+ * @flag: indicates if the sting is a palindrome
+ */
+void check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
+
+/**
+ * is_palindrome - checks for palindrome
+ * @s: string
+ *
+ * Return: 1 if palindrome
+ */
+int is_palindrome(char *s)
+{
+	int flag = 1;
+
+	check(s, 0, _strlen_recursion(s) - 1, &flag);
+	return (flag);
+}
